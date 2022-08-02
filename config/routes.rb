725a6ee4019c3ce9c530ed12e3878f356f2c21b1
Sam McClenaghan <sam@aream.ca>
2022-08-02 13:27:14 -0700

    Succesfully added devise and Users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+  end
+  
   devise_for :users
   resources :sprints
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
