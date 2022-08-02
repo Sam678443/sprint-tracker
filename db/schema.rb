3fc588ab0f5afe93bde00d1e08ef08620071a938
Sam McClenaghan <sam@aream.ca>
2022-08-02 12:33:35 -0700

    Created devise views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_02_180023) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_02_193029) do
   create_table "sprints", force: :cascade do |t|
     t.string "name"
     t.boolean "warmup"
@@ -19,6 +19,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_02_180023) do
     t.integer "sprintingbpm"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
